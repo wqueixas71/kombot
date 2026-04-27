4199d5974a4811501b53f5340abc9f58a1265841
wqueixas71 <wqueixas@gmail.com>
2026-04-27 17:30:40 -0300

    ci: add logs folder to before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,5 +3,8 @@ set -e
 
 systemctl stop kombot || true
 mkdir -p /opt/kombot
+mkdir -p /opt/kombot/logs
+touch /opt/kombot/logs/app.log
+
 chown -R kombot:kombot /opt/kombot
 chmod -R 775 /opt/kombot
